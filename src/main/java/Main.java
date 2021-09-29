e025a4987c4f38cc09e9ef5dcd75eabfc297406d
Ed <85837973+bigsurmoon@users.noreply.github.com>
2021-09-29 16:23:15 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
-
     long amount = 1000_60;
     boolean registered = true;
-
     double bonus = service.calculate(amount, registered);
     System.out.println(bonus);
   }
